65f201a054268afb802269e6f92fe34658ec3ec6
Viswada <yangalav@go.stockton.edu>
2024-08-07 19:18:27 -0400

    revert: adding bottom navigation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import Homepage from './Components/Homepage';
-import Box from '@mui/material/Box';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import RestoreIcon from '@mui/icons-material/Restore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
 // import logo from './logo.svg';
+import FixedBottomNavigation from './Components/FixedBottomNavigation/FixedBottomNavigation';
+
 import './App.css';
 
 function App() {
-  const [value, setValue] = React.useState(0);
-
   return (
     <div className="App">
       <Homepage />
@@ -29,19 +23,6 @@ function App() {
           Learn React
         </a>
       </header> */}
-      <Box sx={{ width: 500 }}>
-        <BottomNavigation
-          showLabels
-          value={value}
-          onChange={(event, newValue) => {
-            setValue(newValue);
-          }}
-        >
-          <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-          <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-          <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
-        </BottomNavigation>
-      </Box>
     </div>
   );
 }
