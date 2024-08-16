05d99e56167de96aeccf3a5451b3ee7a37520cb1
Viswada <yangalav@go.stockton.edu>
2024-08-16 12:21:15 -0400

    feat: hook up and test animation pixi js

--- src/Components/RotatingPoop.jsx
+++ src/Components/RotatingPoop.jsx
@@ -2,25 +2,36 @@
 import React from 'react';
 import { useState } from 'react';
 import { Sprite, Stage, Container, useTick } from '@pixi/react';
+import { Texture } from 'pixi.js';
 import Poo1 from '../Assets/poop1.png';
 import Poo2 from '../Assets/poop2.png';
 import Poo3 from '../Assets/poop3.png';
+import PooTest2 from '../Assets/poopTest2.png';
+import PoopTest from '../Assets/poopTest.svg';
+import PoopTest2 from '../Assets/poopTest2.svg';
 
 
-const Poop = ({ x, y, img }) => {
+const Poop = ({ x, y }) => {
   const [rotation, setRotation] = useState(0);
+  const [img, setImage] = useState(Poo2);
 
   useTick((delta) => delta && setRotation(rotation + 0.1 * delta));
+  
+  // let locktesttexture = async () => await PIXI.Texture.fromURL(locktestsvg, {resolution: 10});
+  const texture = Texture.from('../Assets/poopTest.svg', {resolution: 10});
 
   return (
     <>
       <Sprite
         image={img}
         anchor={0.5}
-        scale={1}
-        rotation={rotation}
+        // scale={1}
+        interactive={true}
+        // rotation={rotation}
         x={x}
         y={y}
+        pointerover={() => setImage(PooTest2)}
+        pointerout={() => setImage(Poo2)}
       />
     </>
   );
@@ -29,9 +40,9 @@ const Poop = ({ x, y, img }) => {
 const RotatingPoop = () => (
   <Stage width={500} height={500} options={{ backgroundColor: 16777215 }}>
     <Container position={[250, 250]}>
-      <Poop img={Poo1} x={-50} y={50}/>
-      <Poop img={Poo2} x={50} y={50}/>
-      <Poop img={Poo3} x={150} y={50}/>
+      <Poop />
+      {/* <Poop img={Poo2} x={50} y={50}/> */}
+      {/* <Poop img={Poo3} x={150} y={50}/> */}
     </Container>
   </Stage>
 );
