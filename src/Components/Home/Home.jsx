3f0457974eb0a200b71fcd853d51b9149ab4fbb8
Viswada <yangalav@go.stockton.edu>
2024-08-16 12:13:36 -0400

    feat: flesh out home landing page

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,73 @@
+import Grid from '@mui/material/Unstable_Grid2';
+import Box from '@mui/material/Box';
+import ButtonComponent from '../ButtonComponent';
+import Blurb from '../Blurb/Blurb';
+import PruneHorns from '../../Assets/prune-horns.svg';
+
+import './Home.css';
+
+const headline = 'Save Poo-litics';
+const description = (
+  <div>
+    <p>
+      Every 4 years, sh*tizens of Poo-litics vote for their leader, but somehow always end up with Two-Horns or Wrinkled Prune.
+    </p>
+    <p>
+      See how ranked choice voting effects how represented sh*tizens are compared with the current voting process.
+    </p>
+  </div>
+)
+
+const footNote = 'Play time: 10 min      Oct 2024';
+
+export default function Home() {
+  return (
+    <>
+      <Box
+        sx={{
+          marginTop: 10,
+          marginBottom: 10,
+          textAlign: 'center',
+          flexGrow: 1,
+          maxWidth: 700,
+          paddingLeft: '8%',
+          paddingRight: '8%'
+        }}
+      >
+        <Grid>
+          <img src={PruneHorns} alt="" />
+        </Grid>
+        <Grid>
+          <Blurb
+            headline={headline}
+            description={description}
+            descriptionStyles={{ fontWeight: 400 }}
+          />
+        </Grid>
+        <Grid>
+          <Blurb
+            description={footNote}
+            descriptionStyles={{ fontSize: 15, color: 'grey', fontWeight: 400 }}
+          />
+        </Grid>
+        <Grid>
+          <ButtonComponent 
+            variant="contained"
+            label="Save Poo-litics" 
+            buttonStyles={{ 
+              padding: '16px 24px',
+              marginTop: 5,
+              fontWeight: 700,
+              borderRadius: '10px',
+              color: 'white',
+              backgroundColor: '#6C2023',
+              fontFamily: 'Figtree',
+              textTransform: 'none',
+              fontSize: '16px'
+            }}
+          />
+        </Grid>
+      </Box>
+    </>
+  )
+}
