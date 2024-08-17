d857884b5dfa236e648fdb0db1d1c5d35f259979
Viswada <yangalav@go.stockton.edu>
2024-08-16 23:45:15 -0400

    refactor: add more universal styles to button

--- src/Components/ButtonComponent.jsx
+++ src/Components/ButtonComponent.jsx
@@ -1,9 +1,25 @@
 import Button from '@mui/material/Button';
 
-export default function ButtonComponent({ variant, buttonStyles, label }) {
+export default function ButtonComponent({ href, buttonStyles, label }) {
   return (
     <>
-      <Button disableElevation variant={variant} sx={{ ...buttonStyles }}>{label}</Button>
+      <Button 
+        disableElevation
+        href={href}
+        variant="contained"
+        sx={{ 
+          fontFamily: 'Figtree',
+          fontSize: '16px',
+          padding: '16px 24px',
+          textTransform: 'none',
+          marginTop: 5,
+          fontWeight: 700,
+          borderRadius: '10px',
+          ...buttonStyles
+        }}
+      >
+        {label}
+      </Button>
     </>
   )
 }
